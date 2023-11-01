62a48052ce11e907959dbf40c9467ca5e41f616d
xiaohu <144608259+duongtran0503@users.noreply.github.com>
2023-11-01 21:06:52 +0700

    Update proudct.js

--- js/Data/proudct.js
+++ js/Data/proudct.js
@@ -1,76 +1,11 @@
-///thuộc tính image để trống 
-const product = [
-  {
-    productType: "điện thoại",
-    title: "samsum galazxy",
-    image: `../image/product/dien_thoai/app-img.png`,
-    cont: 53000000,
-    detail:
-      "Mua Samsung Galaxy S23 giá rẻ, chính hãng, giảm ngay 1 triệu, thu cũ đổi mới đến 3 triệu, góp 0%, BH chính hãng 1 năm (đến hết 23/03)",
-    trademark: "samsum",
-  },
-  {
-    productType: "điện thoại",
-    title: "samsum galazxy",
-    image: `../image/product/dien_thoai/app-img.png`,
-    cont: 23000000,
-    detail:
-      "Mua Samsung Galaxy S23 giá rẻ, chính hãng, giảm ngay 1 triệu, thu cũ đổi mới đến 3 triệu, góp 0%, BH chính hãng 1 năm (đến hết 23/03)",
-    trademark: "samsum",
-  },
-  {
-    productType: "điện thoại",
-    title: "samsum galazxy",
-    image: `../image/product/dien_thoai/app-img.png`,
-    cont: 23000000,
-    detail:
-      "Mua Samsung Galaxy S23 giá rẻ, chính hãng, giảm ngay 1 triệu, thu cũ đổi mới đến 3 triệu, góp 0%, BH chính hãng 1 năm (đến hết 23/03)",
-    trademark: "samsum",
-  },
-  {
-    productType: "điện thoại",
-    title: "samsum galazxy",
-    image: `../image/product/dien_thoai/app-img.png`,
-    cont: 23000000,
-    detail:
-      "Mua Samsung Galaxy S23 giá rẻ, chính hãng, giảm ngay 1 triệu, thu cũ đổi mới đến 3 triệu, góp 0%, BH chính hãng 1 năm (đến hết 23/03)",
-    trademark: "samsum",
-  },
-  {
-    productType: "điện thoại",
-    title: "samsum galazxy",
-    image: `../image/product/dien_thoai/app-img.png`,
-    cont: 23000000,
-    detail:
-      "Mua Samsung Galaxy S23 giá rẻ, chính hãng, giảm ngay 1 triệu, thu cũ đổi mới đến 3 triệu, góp 0%, BH chính hãng 1 năm (đến hết 23/03)",
-    trademark: "samsum",
-  },
-  {
-    productType: "điện thoại",
-    title: "samsum galazxy",
-    image: `../image/product/dien_thoai/app-img.png`,
-    cont: 23000000,
-    detail:
-      "Mua Samsung Galaxy S23 giá rẻ, chính hãng, giảm ngay 1 triệu, thu cũ đổi mới đến 3 triệu, góp 0%, BH chính hãng 1 năm (đến hết 23/03)",
-    trademark: "samsum",
-  },
-  {
-    productType: "điện thoại",
-    title: "samsum galazxy",
-    image: `../image/product/dien_thoai/app-img.png`,
-    cont: 23000000,
-    detail:
-      "Mua Samsung Galaxy S23 giá rẻ, chính hãng, giảm ngay 1 triệu, thu cũ đổi mới đến 3 triệu, góp 0%, BH chính hãng 1 năm (đến hết 23/03)",
-    trademark: "samsum",
-  },
-  {
-    productType: "điện thoại",
-    title: "samsum galazxy",
-    image: `../image/product/dien_thoai/app-img.png`,
-    cont: 23000000,
-    detail:
-      "Mua Samsung Galaxy S23 giá rẻ, chính hãng, giảm ngay 1 triệu, thu cũ đổi mới đến 3 triệu, góp 0%, BH chính hãng 1 năm (đến hết 23/03)",
-    trademark: "samsum",
-  },
-];
+///ảnh  để trong thư mục riêng 
+const product = {
+   keyandmouse:[],
+   phone:[],
+  watch:[],
+  speaker:[],
+  Laptop:[],
+  tivi:[],
+  
+}
 export default product;
